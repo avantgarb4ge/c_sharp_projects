f35a2372053cd6079fe554542d1ffa32c2ea4d38
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-19 18:30:17 -0800

    welcome_acme_assignment
    create welcome message and utlilize escape sequence

---
+++ WelcomeAcme/WelcomeAcme/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems. \nRemember, we are accounting on you!");
+        Console.ReadLine();
+    }
+}
