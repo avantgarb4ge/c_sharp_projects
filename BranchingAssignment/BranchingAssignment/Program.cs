0a4a481d2a5af8f1ba7ffcdfd9a3e2d1ae1880e9
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-21 21:18:18 -0800

    comments

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -13,12 +13,14 @@ namespace BranchingAssignment
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("What is the weight of your package? Please use lbs.");
             int weight = Convert.ToInt32(Console.ReadLine());
+            // if statement to split program path if weight is greater than 50
             if (weight > 50)
             {
                 Console.WriteLine("The package is too heavy to ship through Package Express. Try UPS.");
                 Console.ReadLine();
             }
 
+            // if weight is 50 or less
             else
             {
                 Console.WriteLine("What is the width of the package?");
@@ -28,6 +30,7 @@ namespace BranchingAssignment
                 Console.WriteLine("What is the length of the package?");
                 int length = Convert.ToInt32(Console.ReadLine());
                 int dimension = length + width + height;
+                // another if statement if dimensions are too large
                 if (dimension > 50)
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express.");
