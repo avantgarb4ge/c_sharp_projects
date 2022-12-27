98841f65c1d8bdf5ce90bff847b3eb2b1bf00596
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-26 22:45:22 -0800

    Method Submission Assignment

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/MathOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class MathOperation
+    {
+        // passes in two paramerts, sets num2 to a default if user opts out of second input
+        public int operation(int num1, int num2 = 10)
+        {
+            int total = num1 + num2;
+            return total;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation part1 = new MathOperation();
+            Console.WriteLine("Please enter two numbers, one at a time. Note, the second number is optional.");
+            // registers first user input
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // registers second user input, but as string
+            string num2 = Console.ReadLine();
+            // if num2 is blank, the use default number and only pass in num1 argument
+            if (num2 == "")
+            {
+                Console.WriteLine(num1 + " plus 10 (default) equals " + part1.operation(num1));
+            }
+            // if num2 is inputted, pass in both arguments
+            else
+            {
+                int newnum2 = Convert.ToInt32(num2);
+                Console.WriteLine(num1 + " plus " + newnum2 + " equals " + part1.operation(num1, newnum2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
