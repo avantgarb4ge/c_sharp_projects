7fa809b2e745730837e10e9bd1fa3e75e1b01239
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-26 21:40:38 -0800

    Methods Assignment

---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Travel trip1 = new Travel();
+            Console.WriteLine("How many gallons of gas do you need?");
+            trip1.gas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(trip1.gas + " gallons of gas in Oregon would cost $" + trip1.oregon());
+            Console.WriteLine(trip1.gas + " gallons of gas in Maryland would cost $" + trip1.maryland());
+            Console.WriteLine(trip1.gas + " gallons of gas in Texas would cost $" + trip1.texas());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Travel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Travel
+    {
+        public int gas { get; set; } // constructor
+        
+        public int oregon() // method 1: calculate gas total from user's input
+        {
+            return (int)(gas * 4.1);
+        }
+        public int maryland()
+        {
+            return (int)(gas * 3.75);
+        }
+        public int texas()
+        {
+            return (int)(gas * 2.34);
+        }
+    }
+}
