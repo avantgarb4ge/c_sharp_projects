ad536056493fd010dd401d661f1af30d2e056e69
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-26 22:24:26 -0800

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Methods
+    {
+        // first method
+        public int mathOperation(int num)
+        {
+            int total = num + 45;
+            return total;
+        }
+        
+        // second method takes in decimal
+        public int mathOperation(double num)
+        {
+            int num1 = Convert.ToInt32(num);
+            int total = (int)(num1 - 2.2); // explicit cast? not sure why this removes the red squiggly
+            return total;
+        }
+
+        // third method takes in string
+        public int mathOperation(string num)
+        {
+            int num1 = Convert.ToInt32(num);
+            int total = num1 + 3;
+            return total;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates new object
+            Methods unit = new Methods();
+            // calls first method
+            Console.WriteLine("The first method total is: " + unit.mathOperation(5));
+            Console.ReadLine();
+
+            Methods unit1 = new Methods();
+            // calls second method
+            Console.WriteLine("The second method total is: " + unit1.mathOperation(5.8));
+            Console.ReadLine();
+
+            Methods unit2 = new Methods();
+            // calls third method
+            Console.WriteLine("The third method total is: " + unit2.mathOperation("11"));
+            Console.ReadLine();
+        }
+    }
+}
