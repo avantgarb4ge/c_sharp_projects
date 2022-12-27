7e00aa4149d0000f682fbe83580258ea968b8abc
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2022-12-26 23:34:30 -0800

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Class1
+    {
+        public void vMethod(int p1, int p2) //void method
+        {
+            int p3 = (p1 + 15); // performs a math operation on p1 and assigns it to p3 variable    
+            Console.WriteLine(p2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 thing = new Class1(); // instantiates object
+            thing.vMethod(10, 20); //calling void method
+            thing.vMethod(p2: 35, p1: 5); // calling void method with named parameters
+            Console.ReadLine();
+        }
+    }
+}
