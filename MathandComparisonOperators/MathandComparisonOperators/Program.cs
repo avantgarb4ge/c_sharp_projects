4fdf7744690abc9e6b8ebfe64e4a7b437c469ed8
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2023-01-02 17:06:59 -0800

    others

---
+++ MathandComparisonOperators/MathandComparisonOperators/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathandComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MATH AND COMPARISON OPERATORS
+            //int total = 5 + 10;
+
+            // ARRAYS
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 10;
+            //numArray[2] = 15;
+            //numArray[3] = 20;
+            //numArray[4] = 25;
+
+            //int[] numArray1 = new int[] {5, 10, 15, 20, 25 };
+            //int[] numArray2 = {5, 10, 15, 20, 25 };
+
+            //Console.WriteLine(numArray2[2]);
+            //Console.ReadLine();
+
+            // LISTS
+
+            // ITERATIONS
+            //int[] testScores = { 76, 77, 80, 96, 82, 92, 93, 87 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //string[] names = { "Kat", "Kathy", "Katherine", "KP", "KatPak" };
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    Console.WriteLine(names[j]);
+            //}
+            //Console.ReadLine();
+
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(86);
+            //testScores.Add(73);
+            //testScores.Add(93);
+            //testScores.Add(85);
+            //testScores.Add(77);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing Score is: " + score);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<string> names = new List<string>();
+            //names.Add("Kat");
+            //names.Add("Katherine");
+            //names.Add("Kathy");
+            //names.Add("Katie");
+
+            //foreach (string name in names)
+            //{
+            //    if (name == "Kathy")
+            //    {
+            //        Console.WriteLine(name);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<int> testScores = new List<int>() { 95, 93, 86, 94, 78 };
+            //List<int> passingScores = new List<int>();
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        passingScores.Add(score);
+            //    }
+            //}
+
+            //Console.WriteLine(passingScores.Count);
+            //Console.ReadLine();
+
+            // EXCEPTION HANDLING
+
+            //try
+            //{
+            //    Console.WriteLine("Pick a number.");
+            //    int numberOne = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Pick a second number.");
+            //    int numberTwo = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Dividing the two...");
+            //    int numberThree = numberOne / numberTwo;
+            //    Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            //    Console.ReadLine();
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Please type a whole number.");
+            //    return; // returns user to main page. doesnt display message
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Please don't divide by zero.");
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message); // writes error message to the screen
+            //}
+            //finally // guarantees that this line will be called. typically used to log (but not limited to)
+            //{
+            //    Console.ReadLine();
+            //}
+
+            // 
+        }
+    }
+}
