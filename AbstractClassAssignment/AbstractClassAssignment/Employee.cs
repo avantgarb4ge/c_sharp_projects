25b2d3705a7ab2f28419f4f517e6ea2b202da5c2
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2023-01-04 14:04:43 -0800

    polymorphism edit

--- AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace AbstractClassAssignment
 {
-    public class Employee : Person // inherits from Person class
+    public class Employee : Person, IQuittable // inherits from Person class
     {
         public override void sayName() // inherited method, but overwritten
         {
             Console.WriteLine("This the full name: ");
             base.sayName(); // automatic, but i suppose this still executes the method procedure in Person class
+        }
+        public void Quit(Person person)
+        {
+            System.Environment.Exit(0);
         }
     }
 }
