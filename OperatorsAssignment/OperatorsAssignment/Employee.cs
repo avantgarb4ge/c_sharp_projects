5bf071512310372c553b5dc024d00e5b17863c52
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2023-01-04 18:06:37 -0800

    Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // overloading == to compare id properties of two objects
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            if (emp1.id == emp2.id) // if both values equal each other (data type and value)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // wont compile unless the != isnt overloaded as well
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            if (!emp1.id.Equals(emp2.id)) // utilizing built in method, have to note not equal by using !
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating object and its properties
+            Employee emp1 = new Employee();
+            emp1.id = 01;
+            emp1.firstName = "bob";
+            emp1.lastName = "the builder";
+
+            Employee emp2 = new Employee();
+            emp2.id = 01;
+            emp2.firstName = "thomas";
+            emp2.lastName = "the train";
+
+            // comparing values, then adjusting output to console depending on result
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("These employees have the same id number.");
+            }
+            else
+            {
+                Console.WriteLine("These employees have different id numbers.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
