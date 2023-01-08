4702d45c789a97e77d4e05d26be2af1e20dd8519
avantgarb4ge <112592431+avantgarb4ge@users.noreply.github.com>
2023-01-07 22:30:23 -0800

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Person
+    {
+        //Properties of user
+        public string ownerName { get; set; }
+        public string petName { get; set; }
+
+        public Person(string ownerName) : this(ownerName, "Garfield") // constructor with default pet name chained
+        {
+
+        }
+
+        public Person(string ownerName, string petName)  //main constructor
+        {
+            this.ownerName = ownerName;
+            this.petName = petName;
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string petName = "Chowder"; // const variable
+            var faveTreat = "Freeze Dried Chicken"; // variable with var keyword
+
+            Person owner = new Person("Buddy"); // instantiates user class and utilizes chained constructor
+        }
+    }
+}
